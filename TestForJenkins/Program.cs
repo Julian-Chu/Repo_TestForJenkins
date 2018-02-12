fae996b92f71cb39e01ffcc900f158403585f9bf
Yu-Lang Chu <ychu@redheads.de>
2018-02-12 14:55:01 +0100

    Add project files.

---
+++ TestForJenkins/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestForJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
